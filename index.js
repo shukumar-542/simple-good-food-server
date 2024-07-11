c7890cd923084f25c780e5eb5f53f14c2155f702
shukumar-542 <shukumar542@gmail.com>
2024-07-11 18:47:53 +0600

    initialized simply good food server and get all product

---
+++ index.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const { MongoClient, ObjectId } = require('mongodb');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection URL
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        // Connect to MongoDB
+        await client.connect();
+        console.log("Connected to MongoDB");
+
+
+        const db = client.db('simple-food');
+        const productCollection = db.collection('products');
+        const userCollection = db.collection('users');
+        const orderCollection = db.collection('order');
+
+
+        // User Registration
+        app.post('/api/v1/register', async (req, res) => {
+            const { name, email, password } = req.body;
+
+            // Check if email already exists
+            const existingUser = await userCollection.findOne({ email });
+            if (existingUser) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'User already exists'
+                });
+            }
+
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Insert user into the database
+            await userCollection.insertOne({ name, email, password: hashedPassword, role: 'user' });
+
+            res.status(201).json({
+                success: true,
+                message: 'User registered successfully'
+            });
+        });
+
+
+
+        // User Login
+        app.post('/api/v1/login', async (req, res) => {
+            const { email, password } = req.body;
+
+            // Find user by email
+            const user = await userCollection.findOne({ email });
+            if (!user) {
+                return res.status(401).json({ message: 'Invalid email or password' });
+            }
+
+            // Compare hashed password
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: 'Invalid email or password' });
+            }
+
+            // Generate JWT token
+            const token = jwt.sign({ email: user.email, role: user.role }, process.env.JWT_SECRET, { expiresIn: process.env.EXPIRES_IN });
+
+            res.json({
+                success: true,
+                message: 'Login successful',
+                token
+            });
+        });
+
+
+        
+      
+        // Get all Products
+        app.get('/api/v1/product', async (req, res) => {
+            const result = await productCollection.find().toArray();
+            res.json(result)
+        })
+           // get product searching by Id
+           app.get('/api/v1/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productCollection.findOne(query)
+            res.json(result)
+        })
+
+
+        
+    
+
+        // Start the server
+        app.listen(port, () => {
+            console.log(`Server is running on `);
+        });
+
+    } finally {
+    }
+}
+
+run().catch(console.dir);
+
+// Test route
+app.get('/', (req, res) => {
+    const serverStatus = {
+        message: 'Server is running smoothly',
+        timestamp: new Date()
+    };
+    res.json(serverStatus);
+});
